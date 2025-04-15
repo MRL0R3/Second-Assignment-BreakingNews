bacd58b932bc5d4f93abcf01873775e8ffa96fa6
MRL0R3 <ahmadrezajafary1384@gmail.com>
2025-04-15 15:05:57 +0330

    methods are modified and constructor is more efficient now

--- BreakingNews/src/main/java/AP/Infrastructure.java
+++ BreakingNews/src/main/java/AP/Infrastructure.java
@@ -1,60 +1,105 @@
 package AP;
 
+import com.google.gson.JsonParser;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Infrastructure {
-
-    private final String URL;
+    private static final int MAX_NEWS = 20;
     private final String APIKEY;
-    private final String JSONRESULT;
-    private ArrayList<News> newsList; // TODO: Create the News class
+    private final String URL;
+    private List<News> newsList;
 
-
-    public Infrastructure(String APIKEY) {
-        this.APIKEY = APIKEY;
-        this.URL = "https://newsapi.org/v2/everything?q=tesla&from=2025-02-05&sortBy=publishedAt&apiKey=";
-        this.JSONRESULT = getInformation();
+    public Infrastructure(String apiKey) {
+        this.APIKEY = apiKey;
+        this.URL = "https://newsapi.org/v2/everything?q=tesla&pageSize=" + MAX_NEWS + "&sortBy=publishedAt&apiKey=";
+        this.newsList = new ArrayList<>();
     }
 
-    public ArrayList<News> getNewsList() {
-        return newsList;
-    }
-
-    private String getInformation() {
-        try {
-            HttpClient client = HttpClient.newHttpClient();
-
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(URL + APIKEY))
-                    .build();
-
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
-                return response.body();
-            } else {
-                throw new IOException("HTTP error code: " + response.statusCode());
-            }
-        } catch (Exception e) {
-            System.out.println("!!Exception : " + e.getMessage());
-        }
-        return null;
-    }
-
-    private void parseInformation() {
-        // TODO: Get the first 20 news from the articles array of the json result
-        //  and parse the information of each on of them to be mapped to News class
-        //  finally add them to newsList in this class to display them in the output
+    // Public API methods
+    public List<News> fetchNews() throws IOException, InterruptedException {
+        String jsonData = fetchNewsData();
+        parseNewsData(jsonData);
+        return getNewsList();
     }
 
     public void displayNewsList() {
-        // TODO: Display titles of the news you got from api
-        //  and print them in a way that user can choose one
-        //  to see the full information of the news
+        if (newsList.isEmpty()) {
+            System.out.println("No news articles available.");
+            return;
+        }
+
+        System.out.println("\n📰 Latest News Headlines");
+        System.out.println("-----------------------");
+        for (int i = 0; i < newsList.size(); i++) {
+            System.out.printf("%2d. %s\n", i + 1, truncateTitle(newsList.get(i).getTitle()));
+        }
     }
 
+    // Private helper methods
+    String fetchNewsData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL + APIKEY))
+                .header("Accept", "application/json")
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200) {
+            throw new IOException("API request failed with status code: " + response.statusCode());
+        }
+        return response.body();
+    }
+
+    private void parseNewsData(String jsonData) {
+        JsonObject jsonObject = JsonParser.parseString(jsonData).getAsJsonObject();
+
+        if (jsonObject.get("status").getAsString().equals("error")) {
+            throw new RuntimeException("API Error: " + jsonObject.get("message").getAsString());
+        }
+
+        JsonArray articles = jsonObject.getAsJsonArray("articles");
+        newsList.clear(); // Clear existing news
+
+        for (int i = 0; i < Math.min(articles.size(), MAX_NEWS); i++) {
+            JsonObject article = articles.get(i).getAsJsonObject();
+            newsList.add(createNewsFromArticle(article));
+        }
+    }
+
+    private News createNewsFromArticle(JsonObject article) {
+        return new News(
+                getStringOrDefault(article, "title", "No title available"),
+                getStringOrDefault(article, "description", "No description available"),
+                article.getAsJsonObject("source").get("name").getAsString(),
+                getStringOrDefault(article, "author", "Unknown author"),
+                getStringOrDefault(article, "url", "#"),
+                getStringOrDefault(article, "publishedAt", "Unknown date")
+        );
+    }
+
+    List<News> getNewsList() {
+        return new ArrayList<>(newsList);
+    }
+
+    private String getStringOrDefault(JsonObject jsonObject, String key, String defaultValue) {
+        return jsonObject.has(key) && !jsonObject.get(key).isJsonNull()
+                ? jsonObject.get(key).getAsString()
+                : defaultValue;
+    }
+
+    private String truncateTitle(String title) {
+        final int MAX_TITLE_LENGTH = 60;
+        return title.length() > MAX_TITLE_LENGTH
+                ? title.substring(0, MAX_TITLE_LENGTH - 3) + "..."
+                : title;
+    }
 }
